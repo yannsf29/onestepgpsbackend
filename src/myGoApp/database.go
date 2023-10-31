c96a8688585ffec8a7ee04043311711f5d8bb8e3
yann crenn <yanncrenn@yanns-Air.lan>
2023-10-31 15:18:20 -0700

    removed logs

--- src/myGoApp/database.go
+++ src/myGoApp/database.go
@@ -4,26 +4,25 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func initDB(filepath string) *sql.DB {
-	// Connect to the database
+	
 	db, err := sql.Open("sqlite3", filepath)
 	if err != nil {
-		log.Fatalf("Failed to open database: %v", err)
+		panic(fmt.Sprintf("Failed to open database: %v", err)) 
 	}
 
-	// Create tables if they don't already exist
+
 	createTables(db)
 
 	return db
 }
 
 func createTables(db *sql.DB) {
-	// Create the user preferences table
+
 	_, err := db.Exec(`
         CREATE TABLE IF NOT EXISTS user_preferences (
             id INTEGER PRIMARY KEY,
@@ -34,7 +33,7 @@ func createTables(db *sql.DB) {
     `)
 
 	if err != nil {
-		log.Fatalf("Failed to create tables: %v", err)
+		panic(fmt.Sprintf("Failed to create tables: %v", err)) 
 	}
 }
 
@@ -43,8 +42,7 @@ func getUserPreference(db *sql.DB, id int) (UserPreference, error) {
 	var hiddenDevices string
 
 	err := db.QueryRow("SELECT id, sort_order, hidden_devices, icon FROM user_preferences WHERE id=?", id).Scan(&pref.ID, &pref.SortOrder, &hiddenDevices, &pref.Icon)
-	
-	// Handle potential SQL errors.
+
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return pref, fmt.Errorf("No user preference found for ID %d", id)
@@ -52,56 +50,36 @@ func getUserPreference(db *sql.DB, id int) (UserPreference, error) {
 		return pref, fmt.Errorf("Database error: %v", err)
 	}
 
-	// Handle potential JSON unmarshal errors.
 	if err := json.Unmarshal([]byte(hiddenDevices), &pref.HiddenDevices); err != nil {
 		return pref, fmt.Errorf("Failed to unmarshal hidden devices: %v", err)
 	}
 
-    log.Printf("Fetched user preferences: %+v", pref)
 	return pref, nil
 }
 
 func updateUserPreference(db *sql.DB, pref UserPreference) error {
-    // Marshal the HiddenDevices slice into a JSON string
     hiddenDevicesJSON, err := json.Marshal(pref.HiddenDevices)
     if err != nil {
-        log.Printf("Error marshaling hidden devices: %v", err)
         return err
     }
 
-    // Use the UPDATE SQL statement to update the user_preferences table
     result, err := db.Exec(
         "UPDATE user_preferences SET sort_order = ?, hidden_devices = ?, icon = ? WHERE id = ?",
         pref.SortOrder, string(hiddenDevicesJSON), pref.Icon, pref.ID,
     )
     if err != nil {
-        log.Printf("Error updating user preferences in database: %v", err)
         return err
     }
 
-    // Log the marshaled hidden devices for debugging purposes
-    log.Printf("Marshalled hidden devices: %s", string(hiddenDevicesJSON))
-    log.Printf("Updating user preferences for ID: %d", pref.ID)
-
-    // Check the number of rows affected by the update
     rowsAffected, err := result.RowsAffected()
     if err != nil {
-        log.Printf("Error fetching rows affected: %v", err)
         return err
     }
     if rowsAffected == 0 {
-        // If no rows were affected, it means the record doesn't exist.
-        log.Printf("No records were updated. The user with ID: %d might not exist in the database.", pref.ID)
-        // Handle the case where the user does not exist if necessary
-    } else {
-        log.Printf("Updated user preferences for ID: %d", pref.ID)
     }
 
     return nil
 }
-
-
-
 
 func createUserPreference(db *sql.DB, pref UserPreference) error {
 	_, err := db.Exec("INSERT INTO user_preferences(sort_order, hidden_devices, icon) VALUES (?, ?, ?)", pref.SortOrder, pref.HiddenDevices, pref.Icon)
--- src/myGoApp/main.go
+++ src/myGoApp/main.go
@@ -2,37 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"encoding/json"
-	"log"
 	"net/http"
 	"os"
-    "strings"
-    "encoding/base64"
-    "strconv"
+	"strconv"
+	"strings"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-    apiKey string
-    db     *sql.DB
+	apiKey string
+	db     *sql.DB
 )
-
-func init() {
-	apiKey = os.Getenv("ONESTEPGPS_API_KEY")
-	if apiKey == "" {
-		panic("ONESTEPGPS_API_KEY environment variable not set!")
-	}
-}
-
-func main() {
-    db = initDB("./preferences.db")
-    defer db.Close()
-
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/preferences", handleGetUserPreference) // GET request
-    http.HandleFunc("/preferences/update", handleUpdateUserPreference) // POST request
-
-    log.Fatal(http.ListenAndServe(":8081", nil))
-}
 
 func contains(slice []string, val string) bool {
     for _, item := range slice {
@@ -43,135 +25,132 @@ func contains(slice []string, val string) bool {
     return false
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    setCORSHeaders(w) 
-
-    data, err := FetchData(apiKey)
-    if err != nil {
-        log.Printf("Error fetching data: %v", err)
-        http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
-        return
-    }
-    
-    pref, err := getUserPreference(db, 1)
-    if err != nil {
-        log.Printf("Error fetching user preferences: %v", err)
-        http.Error(w, "Failed to fetch user preferences", http.StatusInternalServerError)
-        return
-    }
-
-    var filteredDevices []Device
-    for _, device := range data.Devices {
-        if !contains(pref.HiddenDevices, device.ID) {
-            filteredDevices = append(filteredDevices, device)
-        }
-    }
-
-    response, err := json.Marshal(ApiResponse{Devices: filteredDevices})
-    if err != nil {
-        log.Printf("Error converting data to JSON: %v", err)
-        http.Error(w, "Failed to convert data to JSON", http.StatusInternalServerError)
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(response)
+func init() {
+	apiKey = os.Getenv("ONESTEPGPS_API_KEY")
+	if apiKey == "" {
+		panic("ONESTEPGPS_API_KEY environment variable not set!")
+	}
 }
 
+func main() {
+	db = initDB("./preferences.db")
+	defer db.Close()
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/preferences", handleGetUserPreference) // GET request
+	http.HandleFunc("/preferences/update", handleUpdateUserPreference) // POST request
+
+	panic(http.ListenAndServe(":8081", nil)) // Changed from log.Fatal
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	setCORSHeaders(w)
+
+	data, err := FetchData(apiKey)
+	if err != nil {
+		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
+		return
+	}
+
+	pref, err := getUserPreference(db, 1)
+	if err != nil {
+		http.Error(w, "Failed to fetch user preferences", http.StatusInternalServerError)
+		return
+	}
+
+    
+
+	var filteredDevices []Device
+	for _, device := range data.Devices {
+		if !contains(pref.HiddenDevices, device.ID) {
+			filteredDevices = append(filteredDevices, device)
+		}
+	}
+
+	response, err := json.Marshal(ApiResponse{Devices: filteredDevices})
+	if err != nil {
+		http.Error(w, "Failed to convert data to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
 
 func handleGetUserPreference(w http.ResponseWriter, r *http.Request) {
-    setCORSHeaders(w)
+	setCORSHeaders(w)
 
-    // Extract user ID from the URL path.
-    parts := strings.Split(r.URL.Path, "/")
-    if len(parts) != 3 {
-        http.Error(w, "Invalid URL format", http.StatusBadRequest)
-        return
-    }
-    idStr := parts[2]
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) != 3 {
+		http.Error(w, "Invalid URL format", http.StatusBadRequest)
+		return
+	}
+	idStr := parts[2]
 
-    // Convert the idStr to an integer
-    id, err := strconv.Atoi(idStr)
-    if err != nil {
-        http.Error(w, "Invalid user ID format", http.StatusBadRequest)
-        return
-    }
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID format", http.StatusBadRequest)
+		return
+	}
 
-    // Define pref here before you attempt to use it.
-    pref, err := getUserPreference(db, id)
-    if err != nil {
-        http.Error(w, "Failed to fetch user preferences", http.StatusInternalServerError)
-        return
-    }
+	pref, err := getUserPreference(db, id)
+	if err != nil {
+		http.Error(w, "Failed to fetch user preferences", http.StatusInternalServerError)
+		return
+	}
 
-    // Convert the icon byte slice to a Base64 string before you marshal it into JSON.
-    base64Icon := base64.StdEncoding.EncodeToString(pref.Icon)
-    pref.Icon = []byte(base64Icon)
+	base64Icon := base64.StdEncoding.EncodeToString(pref.Icon)
+	pref.Icon = []byte(base64Icon)
 
-    response, err := json.Marshal(pref)
-    if err != nil {
-        log.Printf("Failed to marshal user preferences: %v", err)
-        http.Error(w, "Failed to convert user preferences to JSON", http.StatusInternalServerError)
-        return
-    }
+	response, err := json.Marshal(pref)
+	if err != nil {
+		http.Error(w, "Failed to convert user preferences to JSON", http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
 
 func handleUpdateUserPreference(w http.ResponseWriter, r *http.Request) {
-    setCORSHeaders(w)
+	setCORSHeaders(w)
 
-    // Respond to the preflight OPTIONS request
-    if r.Method == "OPTIONS" {
-        w.WriteHeader(http.StatusOK)
-        return
-    }
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
-    // Handle only POST requests for updating preferences
-    if r.Method != "POST" {
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusMethodNotAllowed)
-        w.Write([]byte(`{"error": "Method not allowed"}`))
-        return
-    }
+	if r.Method != "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed"}`))
+		return
+	}
 
-    // Define pref inside this function's scope.
-    var pref UserPreference
+	var pref UserPreference
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&pref)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Bad request data"}`))
+		return
+	}
 
-    // Decode the request body directly without reading it to a separate variable first.
-    decoder := json.NewDecoder(r.Body)
-    err := decoder.Decode(&pref) // Use 'err' variable that is already declared
-    if err != nil {
-        log.Printf("Error decoding user preferences: %v", err)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte(`{"error": "Bad request data"}`))
-        return
-    }
+	err = updateUserPreference(db, pref)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Failed to update user preferences"}`))
+		return
+	}
 
-    // Log the decoded user preference for debugging
-    log.Printf("Decoded user preferences: %+v", pref)
-
-    // Update the user preference in the database
-    err = updateUserPreference(db, pref)
-    if err != nil {
-        log.Printf("Error updating user preferences: %v", err)
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(`{"error": "Failed to update user preferences"}`))
-        return
-    }
-
-    w.Header().Set("Content-Type", "application/json")
-    w.Write([]byte(`{"message": "Preference updated successfully"}`))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "Preference updated successfully"}`))
 }
-
 
 func setCORSHeaders(w http.ResponseWriter) {
-    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Origin, Accept")
 }
-
-
